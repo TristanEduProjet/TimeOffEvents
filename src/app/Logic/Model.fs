d6f5174389690e6b43f10e6f3215089e7dde659d
TristanEduProjet <TristanEduProjet@users.noreply.github.com>
2018-12-03 21:04:10 +0100

    At school:   - Example add command   - Example add test   - Abstract of date-time in logic for tests   - Add temporary state for not crash

--- src/app/Logic/Model.fs
+++ src/app/Logic/Model.fs
@@ -5,20 +5,26 @@ open System
 // Then our commands
 type Command =
     | RequestTimeOff of TimeOffRequest
-    | ValidateRequest of UserId * Guid with
+    | ValidateRequest of UserId * Guid
+    | CancelRequest of UserId * Guid
+    with
     member this.UserId =
         match this with
         | RequestTimeOff request -> request.UserId
         | ValidateRequest (userId, _) -> userId
+        | CancelRequest (userId, _) -> userId
 
 // And our events
 type RequestEvent =
     | RequestCreated of TimeOffRequest
-    | RequestValidated of TimeOffRequest with
+    | RequestValidated of TimeOffRequest
+    | RequestCancelled of TimeOffRequest
+    with
     member this.Request =
         match this with
         | RequestCreated request -> request
         | RequestValidated request -> request
+        | RequestCancelled request -> request
 
 // We then define the state of the system,
 // and our 2 main functions `decide` and `evolve`
@@ -27,15 +33,19 @@ module Logic =
     type RequestState =
         | NotCreated
         | PendingValidation of TimeOffRequest
-        | Validated of TimeOffRequest with
+        | Validated of TimeOffRequest
+        | UnknownState of TimeOffRequest //default //TODO: à supprimer une fois tout implementer
+        with
         member this.Request =
             match this with
             | NotCreated -> invalidOp "Not created"
+            | UnknownState request
             | PendingValidation request
             | Validated request -> request
         member this.IsActive =
             match this with
             | NotCreated -> false
+            | UnknownState _
             | PendingValidation _
             | Validated _ -> true
 
@@ -45,6 +55,8 @@ module Logic =
         match event with
         | RequestCreated request -> PendingValidation request
         | RequestValidated request -> Validated request
+        | _ -> UnknownState event.Request
+        //TODO: requestCancelled ...
 
     let evolveUserRequests (userRequests: UserRequestsState) (event: RequestEvent) =
         let requestState = defaultArg (Map.tryFind event.Request.RequestId userRequests) NotCreated
@@ -57,27 +69,30 @@ module Logic =
     let overlapsWithAnyRequest (otherRequests: TimeOffRequest seq) request =
         false //TODO: write this function using overlapsWith
 
-    let createRequest activeUserRequests  request =
+    let createRequest today activeUserRequests request =
         if request |> overlapsWithAnyRequest activeUserRequests then
             Error "Overlapping request"
         // This DateTime.Today must go away!
-        elif request.Start.Date <= DateTime.Today then
+        elif request.Start.Date <= today then
             Error "The request starts in the past"
         else
             Ok [RequestCreated request]
 
     let validateRequest requestState =
         match requestState with
-        | PendingValidation request ->
-            Ok [RequestValidated request]
-        | _ ->
-            Error "Request cannot be validated"
+        | PendingValidation request -> Ok [RequestValidated request]
+        | _ -> Error "Request cannot be validated"
 
-    let decide (userRequests: UserRequestsState) (user: User) (command: Command) =
+    let cancelRequest requestState =
+        match requestState with
+        | PendingValidation request
+        | Validated request ->  Ok [RequestCancelled request]
+        | _ -> Error "Request cannot be cancelled"
+
+    let decide (today: DateTime) (userRequests: UserRequestsState) (user: User) (command: Command) =
         let relatedUserId = command.UserId
         match user with
-        | Employee userId when userId <> relatedUserId ->
-            Error "Unauthorized"
+        | Employee userId when userId <> relatedUserId -> Error "Unauthorized"
         | _ ->
             match command with
             | RequestTimeOff request ->
@@ -88,7 +103,7 @@ module Logic =
                     |> Seq.where (fun state -> state.IsActive)
                     |> Seq.map (fun state -> state.Request)
 
-                createRequest activeUserRequests request
+                createRequest today activeUserRequests request
 
             | ValidateRequest (_, requestId) ->
                 if user <> Manager then
@@ -96,3 +111,10 @@ module Logic =
                 else
                     let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
                     validateRequest requestState
+
+            | CancelRequest (_, requestId) ->
+                if user = Manager then
+                    Error "Unauthorized"
+                else
+                    let requestState = defaultArg (userRequests.TryFind requestId) NotCreated
+                    cancelRequest requestState
